60ec497471d0d05ec90717ac5ca7e6f74d7cb1c9
pookieebaba <shubham18sood@gmail.com>
2026-04-27 15:33:33 +0530

    Fix API baseURL to use Render directly

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ baseURL: (import.meta.env.VITE_API_URL || 'https://stocksense-api-z399.onrender.com') + '/api' })
 
 // Attach JWT to every request
 api.interceptors.request.use(cfg => {
